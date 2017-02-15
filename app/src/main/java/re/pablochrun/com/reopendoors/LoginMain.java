3657ae4008c476f650a8a31dff95aec25c3fe089
Pablo Covarrubias Herrera <pablo.covarrubias@altran.es>
2017-02-15 09:24:25 +0100

    first design for login screen

--- app/src/main/java/re/pablochrun/com/reopendoors/LoginMain.java
+++ app/src/main/java/re/pablochrun/com/reopendoors/LoginMain.java
@@ -1,7 +1,9 @@
 package re.pablochrun.com.reopendoors;
 
+import android.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class LoginMain extends AppCompatActivity {
 
@@ -9,5 +11,20 @@ public class LoginMain extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_main);
+       //hideActionBar();
     }
+
+    /*
+    public void hideActionBar(){
+        View decorView = getWindow().getDecorView();
+        // Hide the status bar.
+        int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
+        decorView.setSystemUiVisibility(uiOptions);
+        // Remember that you should never show the action bar if the
+        // status bar is hidden, so hide that too if necessary.
+        ActionBar actionBar = getActionBar();
+        if(null != actionBar)
+            actionBar.hide();
+    }
+    */
 }
