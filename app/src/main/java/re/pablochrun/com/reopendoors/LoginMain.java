ed75bcb0baa6ba4d88af1f2322d2e4c4e67be6ad
Pablo Covarrubias Herrera <pablo.covarrubias@altran.es>
2017-02-15 14:47:47 +0100

    Fonts and password added

--- app/src/main/java/re/pablochrun/com/reopendoors/LoginMain.java
+++ app/src/main/java/re/pablochrun/com/reopendoors/LoginMain.java
@@ -1,17 +1,58 @@
 package re.pablochrun.com.reopendoors;
 
 import android.app.ActionBar;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginMain extends AppCompatActivity {
+
+    public static String LOGIN_PASS;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_main);
+
+        initPreferences();
+
+        setFont();
        //hideActionBar();
+    }
+
+    public void initPreferences(){
+        SharedPreferences sharedPref = this.getPreferences(Context.MODE_PRIVATE);
+        if(!sharedPref.contains(LOGIN_PASS)){
+            SharedPreferences.Editor editor = sharedPref.edit();
+            editor.putInt(LOGIN_PASS, 111111);
+            editor.commit();
+        }
+    }
+
+
+    public void setFont(){
+        Button button = (Button) findViewById(R.id.btLogin);
+        Typeface font = Typeface.createFromAsset(getAssets(), "JLSSpaceGothicR_NC.otf");
+        button.setTypeface(font);
+    }
+
+    public boolean checkLogin(){
+        boolean loginOK = false;
+
+        EditText et = (EditText) findViewById(R.id.etPassword);
+        String password = et.getText().toString();
+        SharedPreferences sharedPref = this.getPreferences(Context.MODE_PRIVATE);
+        sharedPref.getInt(LOGIN_PASS,0);
+
+
+
+
+        return loginOK;
     }
 
     /*
