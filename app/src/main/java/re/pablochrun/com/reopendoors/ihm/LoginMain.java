7abf61b3d82799b4b3aaaa63d2d133da44baa5c5
Pablo Covarrubias Herrera <pablo.covarrubias@altran.es>
2017-02-15 15:55:33 +0100

    Password checked and custom toast added

--- app/src/main/java/re/pablochrun/com/reopendoors/ihm/LoginMain.java
+++ app/src/main/java/re/pablochrun/com/reopendoors/ihm/LoginMain.java
@@ -1,14 +1,22 @@
-package re.pablochrun.com.reopendoors;
+package re.pablochrun.com.reopendoors.ihm;
 
 import android.app.ActionBar;
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import re.pablochrun.com.reopendoors.R;
 
 public class LoginMain extends AppCompatActivity {
 
@@ -18,12 +26,12 @@ public class LoginMain extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_main);
-
         initPreferences();
-
         setFont();
        //hideActionBar();
     }
+
+
 
     public void initPreferences(){
         SharedPreferences sharedPref = this.getPreferences(Context.MODE_PRIVATE);
@@ -41,7 +49,7 @@ public class LoginMain extends AppCompatActivity {
         button.setTypeface(font);
     }
 
-    public boolean checkLogin(){
+    public void checkLogin(View v){
         boolean loginOK = false;
 
         EditText et = (EditText) findViewById(R.id.etPassword);
@@ -49,12 +57,33 @@ public class LoginMain extends AppCompatActivity {
         SharedPreferences sharedPref = this.getPreferences(Context.MODE_PRIVATE);
         sharedPref.getInt(LOGIN_PASS,0);
 
-
-
-
-        return loginOK;
+        if( !password.equals(null)
+                && !password.equals("")
+                && Integer.getInteger(password)==sharedPref.getInt(LOGIN_PASS,0)){
+            //NEW ACTIVITY TO ADMIN DOORs PASSWORDS.
+        }
+        else{
+            showCustomToast(R.string.error_login, Toast.LENGTH_LONG, R.color.colorErrorLogin);
+        }
     }
 
+    public void showCustomToast(int messageId,int duration, int color){
+        LayoutInflater inflater=getLayoutInflater();
+        View customToastView =inflater.inflate(R.layout.custom_toast, null);
+
+        TextView text = (TextView) customToastView.findViewById(R.id.custom_toast_tv);
+        text.setText(getResources().getString(messageId));
+        Typeface font = Typeface.createFromAsset(getAssets(), "JLSSpaceGothicR_NC.otf");
+        text.setTypeface(font);
+
+
+
+        Toast customToast=new Toast(this);
+        customToast.setGravity(Gravity.CENTER_HORIZONTAL | Gravity.BOTTOM,0, 0);
+        customToast.setDuration(duration);
+        customToast.setView(customToastView);
+        customToast.show();
+    }
     /*
     public void hideActionBar(){
         View decorView = getWindow().getDecorView();
