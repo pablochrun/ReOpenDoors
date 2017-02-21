903e5d681cb2276a1c95f2722fd21f40103d96a2
Pablo Covarrubias Herrera <pablo.covarrubias@altran.es>
2017-02-21 09:26:45 +0100

    countdown implemented

--- app/src/main/java/re/pablochrun/com/reopendoors/ihm/DoorOnePass.java
+++ app/src/main/java/re/pablochrun/com/reopendoors/ihm/DoorOnePass.java
@@ -1,16 +1,21 @@
 package re.pablochrun.com.reopendoors.ihm;
 
 import android.graphics.Typeface;
+import android.icu.util.TimeUnit;
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.util.Log;
 import android.widget.GridView;
 import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 import re.pablochrun.com.reopendoors.R;
 import re.pablochrun.com.reopendoors.adapter.CustomGridAdapter;
 
 public class DoorOnePass extends DoorPassParent{
 
-
+    TextView tvCountdown;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,6 +43,25 @@ public class DoorOnePass extends DoorPassParent{
         tvUpper.setTypeface(font);
         gv=(GridView) findViewById(R.id.gridView1);
         gv.setAdapter(new CustomGridAdapter(this, values,intValues));
+
+        tvCountdown = (TextView) findViewById(R.id.countdown);
+        tvCountdown.setTypeface(font);
+
+        new CountDownTimer(3600000, 1000) {
+            public void onTick(long millisUntilFinished) {
+                long minutes = java.util.concurrent.TimeUnit.MILLISECONDS.toMinutes(millisUntilFinished);
+                long seconds = (java.util.concurrent.TimeUnit.MILLISECONDS.toSeconds(millisUntilFinished) - minutes*60);
+
+                String countdownUpdated = String.valueOf(minutes) +":"+String.valueOf(seconds);
+                String countdownFormatted = getString(R.string.spreadingIn, countdownUpdated);
+
+                tvCountdown.setText(countdownFormatted);
+            }
+
+            public void onFinish() {
+                tvCountdown.setText(R.string.infectados);
+            }
+        }.start();
     }
 
 
@@ -52,5 +76,7 @@ public class DoorOnePass extends DoorPassParent{
         }
         tv.setText(passWrited);
     }
+
+
 
 }
